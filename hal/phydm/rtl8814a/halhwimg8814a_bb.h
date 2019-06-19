fa402ec1ded27578866efa562b51126c3600b523
Paulo E. Castro <pauloedgarcastro@gmail.com>
2019-06-19 11:28:40 +0100

    Functions and variables-

--- hal/phydm/rtl8814a/halhwimg8814a_bb.h
+++ hal/phydm/rtl8814a/halhwimg8814a_bb.h
@@ -1,21 +1,21 @@
-/****************************************************************************** 
-* 
-* Copyright(c) 2007 - 2011 Realtek Corporation. All rights reserved. 
-* 
-* This program is free software; you can redistribute it and/or modify it 
-* under the terms of version 2 of the GNU General Public License as 
-* published by the Free Software Foundation. 
-* 
-* This program is distributed in the hope that it will be useful, but WITHOUT 
-* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or 
-* FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for 
-* more details. 
-* 
-* You should have received a copy of the GNU General Public License along with 
-* this program; if not, write to the Free Software Foundation, Inc., 
-* 51 Franklin Street, Fifth Floor, Boston, MA 02110, USA 
-* 
-* 
+/******************************************************************************
+*
+* Copyright(c) 2007 - 2011 Realtek Corporation. All rights reserved.
+*
+* This program is free software; you can redistribute it and/or modify it
+* under the terms of version 2 of the GNU General Public License as
+* published by the Free Software Foundation.
+*
+* This program is distributed in the hope that it will be useful, but WITHOUT
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+* FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+* more details.
+*
+* You should have received a copy of the GNU General Public License along with
+* this program; if not, write to the Free Software Foundation, Inc.,
+* 51 Franklin Street, Fifth Floor, Boston, MA 02110, USA
+*
+*
 ******************************************************************************/
 
 /*Image2HeaderVersion: 2.19*/
@@ -30,9 +30,9 @@
 
 void
 odm_read_and_config_mp_8814a_agc_tab(/* TC: Test Chip, MP: MP Chip*/
-	struct dm_struct    *  pDM_Odm
+	struct dm_struct    *dm
 );
-u4Byte ODM_GetVersion_MP_8814A_AGC_TAB(void);
+u4Byte odm_get_version_mp_8814a_agc_tab(void);
 
 /******************************************************************************
 *                           PHY_REG.TXT
@@ -40,9 +40,9 @@ u4Byte ODM_GetVersion_MP_8814A_AGC_TAB(void);
 
 void
 odm_read_and_config_mp_8814a_phy_reg(/* TC: Test Chip, MP: MP Chip*/
-	struct dm_struct    *  pDM_Odm
+	struct dm_struct    *dm
 );
-u4Byte ODM_GetVersion_MP_8814A_PHY_REG(void);
+u4Byte odm_get_version_mp_8814a_phy_reg(void);
 
 /******************************************************************************
 *                           PHY_REG_MP.TXT
@@ -50,9 +50,9 @@ u4Byte ODM_GetVersion_MP_8814A_PHY_REG(void);
 
 void
 odm_read_and_config_mp_8814a_phy_reg_mp(/* TC: Test Chip, MP: MP Chip*/
-	struct dm_struct    *  pDM_Odm
+	struct dm_struct    *dm
 );
-u4Byte ODM_GetVersion_MP_8814A_PHY_REG_MP(void);
+u4Byte odm_get_version_mp_8814a_phy_reg_mp(void);
 
 /******************************************************************************
 *                           PHY_REG_PG.TXT
@@ -60,9 +60,9 @@ u4Byte ODM_GetVersion_MP_8814A_PHY_REG_MP(void);
 
 void
 odm_read_and_config_mp_8814a_phy_reg_pg(/* TC: Test Chip, MP: MP Chip*/
-	struct dm_struct    *  pDM_Odm
+	struct dm_struct    *dm
 );
-u4Byte ODM_GetVersion_MP_8814A_PHY_REG_PG(void);
+u4Byte odm_get_version_mp_8814a_phy_reg_pg(void);
 
 /******************************************************************************
 *                           PHY_REG_PG_Type2.TXT
@@ -70,9 +70,9 @@ u4Byte ODM_GetVersion_MP_8814A_PHY_REG_PG(void);
 
 void
 odm_read_and_config_mp_8814a_phy_reg_pg_type2(/* TC: Test Chip, MP: MP Chip*/
-	struct dm_struct    *  pDM_Odm
+	struct dm_struct    *dm
 );
-u4Byte ODM_GetVersion_MP_8814A_PHY_REG_PG_Type2(void);
+u4Byte odm_get_version_mp_8814a_phy_reg_pg_type2(void);
 
 /******************************************************************************
 *                           PHY_REG_PG_Type3.TXT
@@ -80,9 +80,9 @@ u4Byte ODM_GetVersion_MP_8814A_PHY_REG_PG_Type2(void);
 
 void
 odm_read_and_config_mp_8814a_phy_reg_pg_type3(/* TC: Test Chip, MP: MP Chip*/
-	struct dm_struct    *  pDM_Odm
+	struct dm_struct    *dm
 );
-u4Byte ODM_GetVersion_MP_8814A_PHY_REG_PG_Type3(void);
+u4Byte odm_get_version_mp_8814a_phy_reg_pg_type3(void);
 
 /******************************************************************************
 *                           PHY_REG_PG_Type5.TXT
@@ -90,10 +90,9 @@ u4Byte ODM_GetVersion_MP_8814A_PHY_REG_PG_Type3(void);
 
 void
 odm_read_and_config_mp_8814a_phy_reg_pg_type5(/* TC: Test Chip, MP: MP Chip*/
-	struct dm_struct    *  pDM_Odm
+	struct dm_struct    *dm
 );
-u4Byte ODM_GetVersion_MP_8814A_PHY_REG_PG_Type5(void);
+u4Byte odm_get_version_mp_8814a_phy_reg_pg_type5(void);
 
 #endif
 #endif /* end of HWIMG_SUPPORT*/
-
