5c2d9f4fb119d11009a8eacf6372da6a0f329de9
Paulo E. Castro <pauloedgarcastro@gmail.com>
2019-06-19 11:43:11 +0100

    Retrofitted, Windows specfic ?

--- hal/phydm/halrf/rtl8814a/halrf_8814a_win.h
+++ hal/phydm/halrf/rtl8814a/halrf_8814a_win.h
@@ -18,89 +18,81 @@
  *
  ******************************************************************************/
 
-#ifndef __HAL_PHY_RF_8814A_H__
-#define __HAL_PHY_RF_8814A_H__
+#ifndef __HALRF_8814A_H__
+#define __HALRF_8814A_H__
 
 /*--------------------------Define Parameters-------------------------------*/
 #define AVG_THERMAL_NUM_8814A	4
 
 #include "halphyrf_win.h"
 
-void ConfigureTxpowerTrack_8814A(
-	PTXPWRTRACK_CFG	pConfig
-	);
+void configure_txpower_track_8814a(
+	struct txpwrtrack_cfg	*config
+);
 
-VOID
-GetDeltaSwingTable_8814A(
-	IN 	PDM_ODM_T			pDM_Odm,
-	OUT pu1Byte 			*TemperatureUP_A,
-	OUT pu1Byte 			*TemperatureDOWN_A,
-	OUT pu1Byte 			*TemperatureUP_B,
-	OUT pu1Byte 			*TemperatureDOWN_B	
-	);
+void
+get_delta_swing_table_8814a(
+	void		*dm_void,
+	u8 **temperature_up_a,
+	u8 **temperature_down_a,
+	u8 **temperature_up_b,
+	u8 **temperature_down_b
+);
 
-VOID
-GetDeltaSwingTable_8814A_PathCD(
-	IN 	PDM_ODM_T			pDM_Odm,
-	OUT pu1Byte 			*TemperatureUP_C,
-	OUT pu1Byte 			*TemperatureDOWN_C,
-	OUT pu1Byte 			*TemperatureUP_D,
-	OUT pu1Byte 			*TemperatureDOWN_D	
-	);
+void
+get_delta_swing_table_8814a_path_cd(
+	void		*dm_void,
+	u8 **temperature_up_c,
+	u8 **temperature_down_c,
+	u8 **temperature_up_d,
+	u8 **temperature_down_d
+);
 
-
-VOID
-ODM_TxPwrTrackSetPwr8814A(
-	PDM_ODM_T			pDM_Odm,
-	PWRTRACK_METHOD 	Method,
-	u1Byte 				RFPath,
-	u1Byte 				ChannelMappedIndex
-	);
+void
+odm_tx_pwr_track_set_pwr8814a(
+	void		*dm_void,
+	enum pwrtrack_method	method,
+	u8				rf_path,
+	u8				channel_mapped_index
+);
 
 u1Byte
-CheckRFGainOffset(
-	PDM_ODM_T			pDM_Odm,
-	u1Byte				RFPath
+check_rf_gain_offset(
+	void		*dm_void,
+	u8				rf_path,
 	);
 
 
 //
 // LC calibrate
 //
-void	
-PHY_LCCalibrate_8814A(
-	IN PDM_ODM_T		pDM_Odm
-	);
+void
+phy_lc_calibrate_8814a(
+	void		*dm_void
+);
 
 //
 // AP calibrate
 //
-void	
-PHY_APCalibrate_8814A(		
+void
+phy_iq_calibrate_8814a(
+	void		*dm_void,
+	boolean	is_recovery
+);
+
+void
+phy_dp_calibrate_8814a(
+	void		*dm_void,
+);
+
+void phy_set_rf_path_switch_8814a(
 #if (DM_ODM_SUPPORT_TYPE & ODM_AP)
-	IN PDM_ODM_T		pDM_Odm,
+	struct dm_struct		*dm,
 #else
-	IN	PADAPTER	pAdapter,
+	void	*adapter,
 #endif
-	IN 	s1Byte		delta
-	);
+	boolean		is_main
+);
 
 
-VOID	                                                 
-PHY_DPCalibrate_8814A(                                   
-	IN 	PDM_ODM_T	pDM_Odm                             
-	);
-
-
-VOID PHY_SetRFPathSwitch_8814A(
-#if (DM_ODM_SUPPORT_TYPE & ODM_AP)
-	IN PDM_ODM_T		pDM_Odm,
-#else
-	IN	PADAPTER	pAdapter,
-#endif
-	IN	BOOLEAN		bMain
-	);
-
-								
-#endif	// #ifndef __HAL_PHY_RF_8188E_H__								
-
+#endif	// #ifndef __HAL_PHY_RF_8188E_H__
